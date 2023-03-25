1762249bb84ec4455a2b39018d2b2156d9d8f21c
Ananyobrata Pal <ananyo141@gmail.com>
2023-03-26 03:43:46 +0530

    remove plain auth

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,18 +1,9 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 
 from . import views
 
 urlpatterns = [
-    # authentication
-    path("auth/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("auth/verify/", TokenVerifyView.as_view(), name="token_verify"),
     # oauth
     path("oauth/", views.OAuthCreate.as_view(), name="oauth"),
     path("oauth/profile/", views.OAuthProfile.as_view(), name="oauth_profile"),
