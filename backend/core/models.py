46b8f3a8864d6ea582c792aa0261542cf3899d76
Ananyobrata Pal <ananyo141@gmail.com>
2023-03-25 13:35:39 +0530

    Feat: add user roles

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,15 +1,29 @@
-from django.contrib.auth.models import AbstractUser, timezone
+from enum import Enum
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+class Role(Enum):
+    ADMIN = "admin"
+    CLIENT = "client"
+    SHOPKEEPER = "shopkeeper"
+
 
 # Create your models here.
 class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(_("email address"), unique=True)
     name = models.CharField(max_length=60)
+    role = models.CharField(
+        max_length=50,
+        choices=[(role.value, role.name) for role in Role],
+        default=Role.CLIENT.value,
+    )
+
     # USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ["email", "name"]
 
     def __str__(self):
         return f"{self.email}"
-
--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import User
+from .models import Role, User
 
 
 class UserSerializer(ModelSerializer):
@@ -10,15 +10,17 @@ class UserSerializer(ModelSerializer):
             "password": {"write_only": True},
             "username": {"required": True},
             "email": {"write_only": True},
+            "role": {"write_only": True},
         }
 
-        fields = ("id", "username", "email", "name", "password")
+        fields = ("id", "username", "role", "email", "name", "password")
 
     def create(self, validated_data):
         user = User(
             email=validated_data["email"],
             name=validated_data["name"],
             username=validated_data["username"],
+            role=validated_data.get("role", Role.CLIENT.value),
         )
         # identify password field to be set as hashed password
         user.set_password(validated_data["password"])
@@ -34,4 +36,4 @@ class UserProfileSerializer(UserSerializer):
     class Meta:
         model = User
         extra_kwargs = {"password": {"write_only": True}}
-        fields = ("id", "username", "email", "name", "password")
+        fields = ("id", "username", "role", "email", "name", "password")
